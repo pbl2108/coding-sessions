684a25330eaec3e49687c9249a100914965c0287
ivana gjorgeva <igjorgeva@gmail.com>
2021-02-08 07:36:04 -0500

    Added triangulation test.

--- TDD/Money.Test/MoneyTests.cs
+++ TDD/Money.Test/MoneyTests.cs
@@ -9,8 +9,18 @@ namespace Money.Test
         public void TestMultiplication()
         {
             Dollar five = new Dollar(5);
-            five.Times(2);
-            Assert.Equal(10, five.Amount);
+            Dollar product = five.Times(2);
+
+            Assert.Equal(10, product.Amount);
+            product = five.Times(3);
+            Assert.Equal(15, product.Amount);
+        }
+
+        [Fact(DisplayName = "Checks Equality")]
+        public void TestEquality()
+        {
+            Assert.True(new Dollar(5).Equals(new Dollar(5)));
+            Assert.False(new Dollar(5).Equals(new Dollar(6)));
         }
     }
 }
