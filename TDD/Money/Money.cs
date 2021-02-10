05802b22cc1343941e3fcbe1593219d67f834acd
ivana gjorgeva <igjorgeva@gmail.com>
2021-02-10 07:32:29 -0500

    Completed comparison between Francs and Dollars

--- TDD/Money/Money.cs
+++ TDD/Money/Money.cs
@@ -13,7 +13,7 @@ namespace Money
         public override bool Equals(object obj)
         {
             Money money = (Money)obj;
-            return Amount == money.Amount;
+            return Amount == money.Amount && GetType().Equals(money.GetType());
         }
     }
 }
