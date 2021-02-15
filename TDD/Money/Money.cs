ca2f71eb139201ac564b6414536ff101c31f149c
ivana gjorgeva <igjorgeva@gmail.com>
2021-02-15 09:44:29 -0500

    Added a base constructor. Call factory methods instead of constructors. Finished Chapter 9.

--- TDD/Money/Money.cs
+++ TDD/Money/Money.cs
@@ -8,24 +8,42 @@ namespace Money
 {
     public abstract class Money
     {
+        protected string currency;
+
+        protected int amount;
+
+        public Money(int amount, string currency)
+        {
+            this.amount = amount;
+            this.currency = currency;
+        }
+
+
+
         public abstract Money Times(int multiplier);
 
-        protected int Amount;
 
         public override bool Equals(object obj)
         {
             Money money = (Money)obj;
-            return Amount == money.Amount && GetType().Equals(money.GetType());
+            return amount == money.amount && GetType().Equals(money.GetType());
         }
+
+
 
         public static Money Dollar(int amount)
         {
-            return new Dollar(amount);
+            return new Dollar(amount, "USD");
         }
 
         public static Money Franc(int amount)
         {
-            return new Franc(amount);
+            return new Franc(amount, "CHF");
+        }
+
+        public string Currency()
+        {
+            return currency;
         }
 
     }
