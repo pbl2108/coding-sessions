a6f587839c20bd60b60855494d4dad648cbc3d79
Petre Lukarov <petre.lukarov@caissallc.com>
2021-02-20 10:22:07 -0500

    Introduced expressions (addition) with money. Dealing with expressions for multiple currencies. Reducing money to a reference currency. For these two problems introduced IExpression and Bank. Chapter 12.

---
+++ TDD/Money/Bank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    public class Bank
+    {
+        public Money Reduce(IExpression source, string to)
+        {
+            return Money.Dollar(10);
+        }
+    }
+}
---
+++ TDD/Money/IExpression.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Money
+{
+    public interface IExpression
+    {
+    }
+}
