e173ffec796c34a0c3e50092d83c001a05fce79f
ivana gjorgeva <igjorgeva@gmail.com>
2021-02-26 07:26:24 -0500

    Added polymorphism sample code.

--- TDD/Money/Program.cs
+++ TDD/Money/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Money
 {
@@ -7,7 +8,22 @@ namespace Money
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            PolymorphismSample();
+        }
+
+        static void PolymorphismSample()
+        {
+            var list = new List<IGeometricForm>()
+            {
+                FormsFactory.CreateForm("circle"),
+                FormsFactory.CreateForm("square"),
+                FormsFactory.CreateForm("square")
+            };
+
+            foreach (IGeometricForm item in list)
+            {
+                item.Draw(12);
+            }
         }
     }
 }
