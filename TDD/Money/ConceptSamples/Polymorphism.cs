b65cfbbcf9cc60d52cf64527809c29d5d56a4605
ivana gjorgeva <igjorgeva@gmail.com>
2021-02-26 07:26:24 -0500

    Added polymorphism sample code.

---
+++ TDD/Money/ConceptSamples/Polymorphism.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    public interface IGeometricForm
+    {
+        void Draw(int countForms);
+    }
+
+    public static class FormsFactory
+    {
+        public static IGeometricForm CreateForm(string form)
+        {
+            switch(form)
+            {
+                case "circle":
+                    return new Circle();
+                case "square":
+                    return new Square();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public class Circle : IGeometricForm
+    {
+        public void Draw(int countForms)
+        {
+            System.Console.WriteLine("We drew {0} circles.", countForms);
+        }
+    }
+
+    public class Square : IGeometricForm
+    {
+        public void Draw(int countForms)
+        {
+            System.Console.WriteLine("We drew {0} squares.", countForms);
+        }
+    }
+
+}
