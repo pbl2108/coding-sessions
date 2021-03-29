f7e996c11be069acfa39e956820b02db1614aeb8
ivana gjorgeva <igjorgeva@gmail.com>
2021-03-29 07:37:41 -0400

    WIP. Running multiple tests. Chapter 23. How Suite It Is

--- TDD/xUnitExample/TestCaseTest.py
+++ TDD/xUnitExample/TestCaseTest.py
@@ -1,6 +1,7 @@
 from TestCase import TestCase
 from WasRun import WasRun
 from TestResult import TestResult
+from TestSuite import TestSuite
 
 class TestCaseTest(TestCase):
     def setUp(self):
@@ -27,7 +28,20 @@ class TestCaseTest(TestCase):
         result.testFailed()
         assert "1 run, 1 failed" == result.summary(), "Expected summary to be '1 run, 1 failed', found something else"
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-TestCaseTest("testFailedResult").run()
-TestCaseTest("testFailedResultFormatting").run()
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        result = TestResult()
+        suite.run(result)
+        assert "2 run, 1 failed" == result.summary(), "Expected summary to be '2 run, 1 failed', found something else"
+
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+result= TestResult()
+suite.run(result)
+print(result.summary())
